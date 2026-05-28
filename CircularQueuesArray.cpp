9808edb81d3e407d6ab81f8a93d9cd8227f7255f
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-05-28 21:31:01 +0700

    iterasi dari awal array hingga REAR

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -106,7 +106,18 @@ public:
                 cout << queue_array[FRONT_Position] << " ";
                 FRONT_Position++;
             }
-
             FRONT_Position = 0;
+
+            // iterasi dari awal array hingga REAR
+            while (FRONT_Position <= REAR_Position)
+            {
+                cout << queue_array[FRONT_Position] << " ";
+                FRONT_Position++;
+            }
+            cout << endl;
+        }
+    }
+};
+
 
 
