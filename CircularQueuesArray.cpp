b58dbf5a9002eab4022f18db8ba203349c40336a
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-05-28 21:30:18 +0700

    jika FRONT > REAR, iterasi dari FRONT hingga akhir array

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -95,7 +95,18 @@ public:
                 cout << queue_array[FRONT_Position] << " ";
                 FRONT_Position++;
             }
-
             cout << endl;
         }
+
+        else
+        {
+            // jika FRONT > REAR, iterasi dari FRONT hingga akhir array
+            while (FRONT_Position <= max - 1)
+            {
+                cout << queue_array[FRONT_Position] << " ";
+                FRONT_Position++;
+            }
+
+            FRONT_Position = 0;
+
 
