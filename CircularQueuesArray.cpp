a33a9882f78edf33f872af7487f2f61c288c087e
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-05-28 20:51:28 +0700

    membuat class bernama  queues

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -1,2 +1,9 @@
 #include <iostream>     //library
 using namespace std;
+
+class Queues //membuat class queues
+{
+private:
+    static const int max = 3;
+    int FRONT, REAR;
+    int queue_array[max];
